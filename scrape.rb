c61d72d93b6fb2099a6a92f32e11075a4e1dcc38
Jacob Rapp <jake.rapp322@gmail.com>
2016-09-11 18:50:06 -0700

    HTML cleaner module for response body

--- scrape.rb
+++ scrape.rb
@@ -1,21 +1,27 @@
 require 'rest-client'
 
-response = RestClient.post 'http://www.xyplanningnetwork.com/wp-admin/admin-ajax.php', {action: 'do_ajax_advisor_search', page: '1', amountPerPage: '10', filterCriteria: 'fee-structure', filterValue: 'all' }
 
-module HTMLWhitespaceCleaner
+module HTMLCleaner
   def self.clean(html_string)
-    remove_all_white_space_between_tags(condense_whitespace(html_string)).strip
+    clean_string = remove_any_white_space_between_tags(condense_whitespace(html_string)).strip
+    remove_escape_characters(clean_string)
   end
 
   private
 
-  WHITE_SPACE_BETWEEN_TAGS = /(?<=>)\s+(?=<)/
-
-  def self.remove_all_white_space_between_tags(html_string)
-    html_string.gsub(WHITE_SPACE_BETWEEN_TAGS, "")
+  def self.remove_any_white_space_between_tags(html_string)
+    html_string.gsub(/(?<=>)\s+(?=<)/, "")
   end
 
   def self.condense_whitespace(html_string)
     html_string.gsub(/\s+/, ' ')
   end
+
+  def self.remove_escape_characters(html_string)
+    html_string.gsub(/\\/, '')
+  end
 end
+
+response = RestClient.post 'http://www.xyplanningnetwork.com/wp-admin/admin-ajax.php', {action: 'do_ajax_advisor_search', page: '1', amountPerPage: '10', filterCriteria: 'fee-structure', filterValue: 'all' }
+
+puts pure_html = HTMLCleaner.clean(response.body)
