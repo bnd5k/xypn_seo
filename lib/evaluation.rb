7209ed01c66be2ae3b318d6356b91a3570c199aa
Jacob Rapp <jake.rapp322@gmail.com>
2016-09-15 22:38:32 -0700

    WIP - what data is needed from API JSON reponse?

--- lib/evaluation.rb
+++ lib/evaluation.rb
@@ -1,7 +1,5 @@
 require 'rest-client'
 
-p ENV["PAGESPEED_KEY"]
+response = RestClient.get "https://www.googleapis.com/pagespeedonline/v1/runPagespeed?url=http://AttainableWealthFP.com/&key=#{ENV['PAGESPEED_KEY']}&prettyprint=false"
 
-# response = RestClient.get "https://www.googleapis.com/pagespeedonline/v1/runPagespeed?url=http://AttainableWealthFP.com/&key=#{ENV['PAGESPEED_KEY']}"
-
-# puts response
+response['score'] # overall score
