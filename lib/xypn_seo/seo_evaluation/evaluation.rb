4ec0edc46dd51f8599d92165d12467878079da09
Jacob Rapp <jake.rapp322@gmail.com>
2016-09-20 20:07:40 -0700

    Move advisor list.

--- lib/xypn_seo/seo_evaluation/evaluation.rb
+++ lib/xypn_seo/seo_evaluation/evaluation.rb
@@ -3,7 +3,7 @@ require 'json'
 
 module SiteEval
 
-  def overall_score(url, strategy)
+  def self.overall_score(url, strategy)
     response = RestClient.get("https://www.googleapis.com/pagespeedonline/v1/runPagespeed
                               ?url=#{url}&key=#{ENV['PAGESPEED_KEY']}&prettyprint=false
                               &strategy=#{strategy}")
