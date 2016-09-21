04385f79f77aa4b7b3cf78706edc80a428ffb9f2
Jacob Rapp <jake.rapp322@gmail.com>
2016-09-20 22:21:21 -0700

    Add scopes and params logic to sort asc and desc records

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,18 @@
 class WelcomeController < ApplicationController
 
   def index
-    @websites = Website.all
+    case params['sort']
+    when 'mobile_asc'
+      @websites = Website.mobile_asc
+    when 'mobile_desc'
+      @websites = Website.mobile_desc
+    when 'desktop_asc'
+      @websites = Website.desktop_asc
+    when 'desktop_desc'
+      @websites = Website.desktop_desc
+    else
+      @websites = Website.all
+    end
   end
-  
+
 end
--- lib/xypn_seo/seo_evaluation/evaluation.rb
+++ lib/xypn_seo/seo_evaluation/evaluation.rb
@@ -4,11 +4,23 @@ require 'json'
 module SiteEval
 
   def self.overall_score(url, strategy)
-    response = RestClient.get("https://www.googleapis.com/pagespeedonline/v1/runPagespeed
-                              ?url=#{url}&key=#{ENV['PAGESPEED_KEY']}&prettyprint=false
-                              &strategy=#{strategy}")
+    response = RestClient.get("https://www.googleapis.com/pagespeedonline/v1/runPagespeed?url=#{url}&key=#{ENV['PAGESPEED_KEY']}&prettyprint=false&strategy=#{strategy}")
     parsed = JSON.parse(response.body)
     parsed['score']
   end
 
 end
+
+Website.all.each.with_index do | website, index |
+  if website.url != "UNKNOWN"
+    website.desktop_score = SiteEval.overall_score(website.url, "desktop")
+    website.mobile_score = SiteEval.overall_score(website.url, "mobile")
+    website.save
+    puts "Finished #{index} - website.business"
+  end
+end
+
+# site = Website.find(6)
+# site.desktop_score = SiteEval.overall_score(website.url, "desktop")
+# site.mobile_score = SiteEval.overall_score(website.url, "mobile")
+# site.save
