029dcd206f694ec7627496a511247a02eb146e0a
Jacob Rapp <jake.rapp322@gmail.com>
2016-09-20 19:59:42 -0700

    Fix collection print bug, refacotred API logic

--- lib/xypn_seo/seo_evaluation/evaluation.rb
+++ lib/xypn_seo/seo_evaluation/evaluation.rb
@@ -1,14 +1,14 @@
 require 'rest-client'
+require 'json'
 
 module SiteEval
 
-  def ping_api(url)
+  def overall_score(url, strategy)
     response = RestClient.get("https://www.googleapis.com/pagespeedonline/v1/runPagespeed
-                              ?url=#{url}&key=#{ENV['PAGESPEED_KEY']}&prettyprint=false")
-  end
-
-  def overall_score(pagespeed_response)
-    pagespeed_response['score']
+                              ?url=#{url}&key=#{ENV['PAGESPEED_KEY']}&prettyprint=false
+                              &strategy=#{strategy}")
+    parsed = JSON.parse(response.body)
+    parsed['score']
   end
 
 end
