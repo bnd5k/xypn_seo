639c3e0c71730010692a57f2f8843b68c11696e2
Ben Downey <bd@bendowney.net>
2017-02-08 21:19:52 -0800

    updates scopes to only query for active websites

--- app/models/website.rb
+++ app/models/website.rb
@@ -1,6 +1,7 @@
 class Website < ApplicationRecord
-  scope :desktop_asc, -> { order(desktop_score: :asc) }
-  scope :desktop_desc, -> { order(desktop_score: :desc) }
-  scope :mobile_asc, -> { order(mobile_score: :asc) }
-  scope :mobile_desc, -> { order(mobile_score: :desc) }
+  scope :active, -> { where(active: true) }
+  scope :desktop_asc, -> { active.order(desktop_score: :asc) }
+  scope :desktop_desc, -> { active.order(desktop_score: :desc) }
+  scope :mobile_asc, -> { active.order(mobile_score: :asc) }
+  scope :mobile_desc, -> { active.order(mobile_score: :desc) }
 end
